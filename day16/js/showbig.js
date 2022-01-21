e403e14456a6d5ff01e3742c162a6122e8012bb3
LD221224 <dayeong2815@gmail.com>
2022-01-21 12:47:30 +0900

    day16

---
+++ day16/js/showbig.js
@@ -0,0 +1,16 @@
+// 큰 사진 선택
+let bigPic = document.querySelector("#cup");
+
+// 작은 사진 선택
+let smallPics = document.querySelectorAll(".small");
+
+// onclick 이벤트
+for(let i = 0; i < smallPics.length; i++){
+    // smallPics[i].onclick = showBig; // showBig() 호출
+    smallPics[i].addEventListener("click", showBig);
+}
+
+function showBig(){
+    let newPic = this.src;  // 이벤트가 일어난 대상의 src
+    bigPic.setAttribute("src", newPic);
+}
